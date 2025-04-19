1f67419a744f57e5a88e4f1fb6ef605cbaa81b5c
WiserZhou <wiserzyf@gmail.com>
2025-04-19 17:00:30 +0800

    update

---
+++ push.sh
@@ -0,0 +1,28 @@
+# 执行 git push
+function do_push() {
+    git add .
+    git commit -m "update"
+    export http_proxy=http://192.168.48.17:18000; export https_proxy=http://192.168.48.17:18000
+    while true; do
+        GIT_ASKPASS=/bin/echo GIT_TERMINAL_PROMPT=0 git push -f origin main
+        if [ $? -eq 0 ]; then
+            echo "Push successful!"
+            break
+        else
+            echo "Push failed. Retrying..."
+            sleep 1
+        fi
+    done
+}
+
+
+# 根据命令行参数执行相应操作
+case "$1" in
+    "push")
+        do_push
+        ;;
+    *)
+        echo "Usage: $0 {clone|pull|push}"
+        exit 1
+        ;;
+esac
